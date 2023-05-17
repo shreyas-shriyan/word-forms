8d05d1229f9988a45f91a0b18e1537ecd566220b
shreyas-shriyan <shreyshriyan@gmail.com>
2023-05-17 17:50:43 +0000

    ui fix and clear suggestions on input change

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 import "./style.css";
 
 // elements
-const searchText = document.querySelector("#input");
+const searchBar = document.querySelector("#input");
 const searchButton = document.querySelector("#searchButton");
 const alternateWords = document.querySelector("#alternateWords");
 
 //listeners
 searchButton.addEventListener("click", handleSearchButton);
+searchBar.addEventListener("input", handleSearchInput);
 
 //helpers
 async function handleSearchButton(e) {
   e.preventDefault();
   const url = `${import.meta.env.VITE_API_URL}/wordforms?word=${
-    searchText.value
+    searchBar.value
   }`;
   const response = await fetch(url);
   const responseData = await response.json();
-  alternateWords.innerHTML = responseData.wordForms || "";
+  alternateWords.innerHTML = responseData.wordForms || "No forms found";
+}
+
+function handleSearchInput() {
+  alternateWords.innerHTML = "";
 }
